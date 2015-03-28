91e942ea6a9f9ab35b8de9d070fb376b48450560
Denis Cheremisov <denis@cheremisov.net>
2015-03-28 22:18:40 +0300

    Dynamically build config file

---
+++ scripts/make_config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import virtserial
+
+
+iterator = virtserial.iter_main()
+port_name = next(iterator)
+
+
+ORIG_CONFIG = 'conf/targetdevice.yaml'
+RESULTED_CONFIG = 'test_targetdevice.yaml'
+
+
+with open(ORIG_CONFIG) as f:
+    config = f.read()
+config = config.replace('/dev/pts/19', port_name)
+with open(RESULTED_CONFIG, 'w') as f:
+    f.write(config)
+
+
+for void in iterator:
+    pass
+
+os.remove(RESULTED_CONFIG)
