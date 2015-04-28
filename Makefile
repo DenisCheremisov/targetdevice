51292e258912e8fb77c888f927e4bc37e6d7a55d
Denis Cheremisov <denis@cheremisov.net>
2015-04-28 12:35:16 +0300

    No more dependency on libbsd

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CPP=g++
+CPP=clang++
 LDFLAGS=
 IFLAGS=
 OPTS=-g -O0 -Wall -Werror -pedantic
@@ -6,7 +6,7 @@ COMPILE=$(CPP) $(LDFLAGS) $(IFLAGS) $(OPTS)
 TESTFLAGS=-lboost_unit_test_framework
 
 all: main.cpp targetdevice.o confparser.o runtime.o confbind.o commands.o background.o model.o network.o controller.o
-	$(COMPILE) -o tdevice main.cpp targetdevice.o confparser.o runtime.o confbind.o commands.o background.o model.o network.o controller.o $(TESTFLAGS) -lyaml -lssl -lcrypto -lpthread -lbsd
+	$(COMPILE) -o tdevice main.cpp targetdevice.o confparser.o runtime.o confbind.o commands.o background.o model.o network.o controller.o $(TESTFLAGS) -lyaml -lssl -lcrypto -lpthread
 
 targetdevice.o: targetdevice.cpp targetdevice.hpp
 	$(COMPILE) -c targetdevice.cpp
--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cerrno>
+
 #include <unistd.h>
 #include <sys/syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <fcntl.h>
+
 #include <err.h>
 #include <string>
+#include <sstream>
+#include <fstream>
 #include <exception>
 #include <memory>
 
 #include <pthread.h>
-
-#include <bsd/libutil.h>
 #include <openssl/ssl.h>
 
 #include "confparser.hpp"
@@ -71,10 +77,65 @@ public:
 };
 
 
+class PidfileOpenError: public std::string {
+public:
+    PidfileOpenError(std::string msg): std::string(msg) {}
+    PidfileOpenError(int code): std::string(strerror(code)) {}
+};
+
+
+class PidfileLockError: public std::string {
+public:
+    PidfileLockError(std::string msg): std::string(msg) {}
+    PidfileLockError(int code): std::string(strerror(code)) {}
+};
+
+
+class PidfileOperator {
+private:
+    int fd;
+    PidfileOperator(int _fd): fd(_fd) {}
+
+public:
+    static PidfileOperator *open(const char *filename) {
+        int fd = ::open(filename,
+                  O_WRONLY | O_CREAT | O_TRUNC | O_NONBLOCK,
+                  S_IRUSR | S_IWUSR);
+        if(fd < 0) {
+            throw PidfileOpenError(errno);
+        }
+
+        int status = flock(fd, LOCK_EX | LOCK_NB);
+        if(status) {
+            close(fd);
+            std::ifstream file(filename);
+            std::string buf;
+            file >> buf;
+            throw PidfileLockError(buf);
+        }
+        pwrite(fd, "-1", 2, 0);
+
+        return new PidfileOperator(fd);
+    }
+
+    ~PidfileOperator() throw() {
+        if(fd > 0) {
+            close(fd);
+        }
+    }
+
+    void write(int pid) {
+        std::stringstream buf;
+        buf << pid;
+        std::string data = buf.str();
+        pwrite(fd, data.c_str(), data.size(), 0);
+    }
+};
+
+
 int main(int argc, char **argv) {
     char *config_file_name;
-    pid_t otherpid, childpid;
-    struct pidfh *pfh;
+    pid_t childpid;
 
     bool daemonize = false;
 
@@ -104,24 +165,31 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    auto_ptr<PidfileOperator> pidoperator;
     if(daemonize) {
-        const char *pidfile = init->conf->daemon().pidfile.c_str();
-        pfh = pidfile_open(pidfile, 0600, &otherpid);
-        if(pfh == NULL) {
-            if(errno == EEXIST) {
-                errx(EXIT_FAILURE, "Daemon already running, pidf: %d.",
-                     (int)otherpid);
-            }
-            warn("Cannot open or create pidfile");
+        const char *pidfilename = init->conf->daemon().pidfile.c_str();
+        PidfileOperator *pidop;
+
+        try {
+            pidop = PidfileOperator::open(pidfilename);
+        } catch(PidfileOpenError e) {
+            syslog(LOG_ERR, "Error: %s", e.c_str());
+            return -1;
+        } catch(PidfileLockError e) {
+            syslog(
+                LOG_ERR,
+                "Cannot start daemon since another instance is running: %s",
+                e.c_str());
+            return -1;
         }
 
         if(daemon(0, 0) == -1) {
             warn("Cannot daemonize");
-            pidfile_remove(pfh);
+            delete pidop;
             exit(EXIT_FAILURE);
         }
 
-        pidfile_write(pfh);
+        pidop->write(getpid());
         childpid = fork();
 
         switch(childpid) {
@@ -130,7 +198,8 @@ int main(int argc, char **argv) {
             return -1;
             break;
         case 0:
-            pidfile_write(pfh);
+            pidoperator = auto_ptr<PidfileOperator>(pidop);
+            pidop->write(getpid());
             break;
         default:
             syslog(LOG_INFO, "Child %d started.", (int)childpid);
